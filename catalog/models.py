bbd564c87c1fb8e0bafe1d62a90dccade638f773
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-04-24 19:00:00 +0200

    Add models Specialization and Hall

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Specialization(models.Model):
+    specialization = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.specialization
+
+
+class Hall(models.Model):
+    hall = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.hall
