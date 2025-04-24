31d010f23d0fc51025617c8e05275f4e0571cd24
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-04-24 19:20:21 +0200

    Add explicit master_id as primary key in Master model

--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 class Specialization(models.Model):
-    specialization = models.CharField(max_length=255, unique=True)
+    specialization = models.CharField(max_length=255, primary_key=True, unique=True)
 
     def __str__(self):
         return self.specialization
 
 
 class Hall(models.Model):
-    hall = models.CharField(max_length=10, unique=True)
+    hall = models.CharField(max_length=10, primary_key=True, unique=True)
 
     def __str__(self):
         return self.hall
 
 
 class Master(models.Model):
+    master_id = models.CharField(max_length=10, primary_key=True, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     password = models.CharField(max_length=128)
