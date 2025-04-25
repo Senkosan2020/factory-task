3374f351ee8cd27e37ccf88d13c4be3e3154cefc
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-04-25 18:29:11 +0200

    Add models to admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hall, Specialization, Master, Box, Work, Worker
 
-# Register your models here.
+admin.site.register(Hall)
+admin.site.register(Specialization)
+admin.site.register(Master)
+admin.site.register(Box)
+admin.site.register(Work)
+admin.site.register(Worker)
