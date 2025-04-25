704595bc7e54285531a7b2c0868d0769bf920e3c
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-04-25 18:52:10 +0200

    Rename directory, add crispy and catalog to inst app, add DB

--- factory_task/wsgi.py
+++ factory_task/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for factory-task project.
+WSGI config for factory_task project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'factory-task.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'factory_task.settings')
 
 application = get_wsgi_application()
