4f0a51c483f615129ee9702739d917761db1d747
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-05-21 21:04:29 +0200

    Add DB counts in homepage stats

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Master, Worker, Box
 
 def index(request):
-    return render(request, "factory/index.html")
+    context = {
+        'masters_count': Master.objects.count(),
+        'workers_count': Worker.objects.count(),
+        'boxes_count': Box.objects.count(),
+    }
+    return render(request, "factory/index.html", context)
