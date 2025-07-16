41cec3a7c12eee3735ff6f3715ac55fb51261f3d
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-07-16 19:00:42 +0200

    Preparing to Render

--- catalog/settings/prod.py
+++ catalog/settings/prod.py
@@ -7,6 +7,10 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
