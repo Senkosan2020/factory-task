3752c7dd566ddaeb61b8bc33a09632618c5ec6ce
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-07-17 19:42:46 +0200

    Rewrite prod.py to use SSL

--- catalog/settings/prod.py
+++ catalog/settings/prod.py
@@ -17,10 +17,13 @@ if RENDER_EXTERNAL_HOSTNAME:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER' : os.environ['POSTGRES_USER'],
-        'PASSWORD' : os.environ['POSTGRES_PASSWORD'],
-        'HOST' : os.environ['POSTGRES_HOST'],
-        'PORT' : os.environ['POSTGRES_DB_PORT'],
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
