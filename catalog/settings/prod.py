43d4fd7fc6327992d84aa216052706d688a4f7ec
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-07-17 19:34:13 +0200

    Rewrite prod.py to use postgresql

--- catalog/settings/prod.py
+++ catalog/settings/prod.py
@@ -16,7 +16,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['POSTGRES_DB'],
         'USER' : os.environ['POSTGRES_USER'],
         'PASSWORD' : os.environ['POSTGRES_PASSWORD'],
